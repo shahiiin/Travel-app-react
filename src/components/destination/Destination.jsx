b9810ea459a81a4d3f9b55403420476655bcc4d4
shahin <shahin.shahi1986@gmail.com>
2022-08-02 17:16:29 +0300

    import and style from destination component

--- src/components/destination/Destination.jsx
+++ src/components/destination/Destination.jsx
@@ -12,7 +12,17 @@ const Destination = () => {
 
     return (
         <div name='destinations' className='destinations'>
-            Destination
+            <div className="container">
+                <h1>All-Inclusive Resorts</h1>
+                <p>On the Caribbean's Best Beaches</p>
+                <div className='img-container'>
+                    <img className='span-3 image-grid-row-2' src={BoraBora} alt="/" />
+                    <img src={BoraBora2} alt="/" />
+                    <img src={Maldives} alt="/" />
+                    <img src={Maldives2} alt="/" />
+                    <img src={KeyWest} alt="/" />
+                </div>
+            </div>
         </div>
     )
 }
