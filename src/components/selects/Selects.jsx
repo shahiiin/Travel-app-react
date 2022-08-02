694f2ce2673c7ec26feeea8f6649ef94e9f2a177
shahin <shahin.shahi1986@gmail.com>
2022-08-02 18:14:44 +0300

    select component style is ready

---
+++ src/components/selects/Selects.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import BoraBora from '../../assets/borabora.jpg'
+import BoraBora2 from '../../assets/borabora2.jpg'
+import Maldives from '../../assets/maldives.jpg'
+import Maldives2 from '../../assets/maldives2.jpg'
+import Maldives3 from '../../assets/maldives3.jpg'
+import KeyWest from '../../assets/keywest.jpg'
+import './selectsStyles.css'
+import SelectsImg from '../SelectsImg/SelectsImg'
+
+
+const Selects = () => {
+
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Selects
