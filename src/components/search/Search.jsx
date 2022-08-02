8abe9977db21785c7e40ee9750245d7e2a8b8ae1
shahin <shahin.shahi1986@gmail.com>
2022-08-02 17:48:28 +0300

    make and import in search component

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import './searchStyles.css'
 
 const Search = () => {
-  return (
-    <div>Search</div>
-  )
+    return (
+        <div className='search'>
+            <div className='container'>
+                <div className='left'>
+                    <h2>LUXURY INCLUDED VACATIONS FOR TWO PEOPLE</h2>
+                    <p>Come experience the very pinnacle of luxury Caribbean all-inclusive vacations for couples at Sandals Resorts. Our luxury beach resorts, set along the most gorgeous tropical settings and exquisite beaches in Saint Lucia, Jamaica, Antigua, The Bahamas, Grenada, Barbados and Curaçao, feature unlimited gourmet dining, unique bars serving premium liquors and wines, and every land and water sport, including complimentary green fees at our golf resorts and PADI® certified scuba diving at most resorts. If you are planning a wedding, Sandals is the leader in Caribbean destination weddings and honeymoon packages.</p>
+                </div>
+                <div className='right'>
+
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Search
