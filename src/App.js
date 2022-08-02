c7cbf7c20a5b38e5ac895b428e32e6d058b7149a
shahin <shahin.shahi1986@gmail.com>
2022-08-02 17:08:30 +0300

    make destination styles

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Destination from "./components/destination/Destination";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Navbar/>
       <Hero/>
+      <Destination/>
     </div>
   );
 }
---
+++ src/components/destination/Destination.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./destinationStyles.css"
+import BoraBora from '../../assets/borabora.jpg'
+import BoraBora2 from '../../assets/borabora2.jpg'
+import Maldives from '../../assets/maldives.jpg'
+import Maldives2 from '../../assets/maldives2.jpg'
+import KeyWest from '../../assets/keywest.jpg'
+
+
+
+const Destination = () => {
+
+    return (
+        <div name='destinations' className='destinations'>
+            Destination
+        </div>
+    )
+}
+
+export default Destination
